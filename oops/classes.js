974d82f1daf9a2aca340242594f943a21329632e
rohitrodge <webrohit27@gmail.com>
2024-03-19 19:38:21 +0530

    commit classes

--- oops/classes.js
+++ oops/classes.js
@@ -1,6 +1,28 @@
-function car (name, brand, color, price){
-    
+// class (oops)
+
+class Chair {
+    contructor(height, width, color, weight, material){
+        this.height = height;
+        this.width = width;
+        this.color = color;
+        this.weight = weight;
+        this.material = material;
+    }
+
+    seat(){
+        console.log('sit');
+    }
+
+    adjust_seat(){
+        console.log('adjusting seat');
+    }
+
+    rotate(){
+        console.log('Rotating');
+    }
+     
 }
-
  
+let chair1 = new Chair(3, 2, 'purple', 9, 'fiber');
 
+console.log(chair1);
