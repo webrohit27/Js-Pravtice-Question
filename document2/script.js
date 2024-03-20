e6be78452256990ba5c6cf65a73dbea27559d5b7
rohitrodge <webrohit27@gmail.com>
2024-03-20 20:07:22 +0530

    commit done

---
+++ document2/script.js
@@ -0,0 +1,10 @@
+let box1 = document.getElementById('box1')
+let value = box1.innerText;
+
+
+let box2 = document.getElementById('box2')
+let value1 = box2.innerText;
+
+
+let sum = parseInt (value) + parseInt(value1)
+box3.innerText = sum; 
