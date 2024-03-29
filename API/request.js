703b0b642a85974d31387d80e430da04e4f74014
rohitrodge <webrohit27@gmail.com>
2024-03-29 13:53:41 +0530

    Send request

--- API/request.js
+++ API/request.js
@@ -1,4 +1,15 @@
-const request = new XMLHttpRequest();
+const request = new XMLHttpRequest(); 
 
-request.open('GET','https://jsonplaceholder.typicode.com/todos/');
-request.send();
+request.open('GET','https://jsonplaceholder.typicode.com/todos/'); 
+request.send(); 
+
+
+
+request.addEventListener('readystatechange', ()=>{
+  if(request.readyState == 4 && request.status == 200){
+    console.log(request.responseText);
+  }
+});
+
+
+
