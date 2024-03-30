7e10fd6c8ce40e506287b06f8c11e0eefd4bace7
rohitrodge <webrohit27@gmail.com>
2024-03-30 23:01:52 +0530

    update callback

---
+++ API/callback.js
@@ -0,0 +1,34 @@
+
+function getTodos(callback){
+    const request = new XMLHttpRequest(); // state 0
+
+    request.open('GET','https://jsonplaceholder.typicode.com/todos/'); 
+    request.send(); 
+    
+    
+    
+    request.addEventListener('readystatechange', ()=>{
+      if(request.readyState == 4 && request.status == 200){
+       callback(undefined, request.responseText);
+      }
+      else if(request.readyState == 4){
+        callback(request.status, undefined);
+      }
+    });
+}
+
+getTodos((err, data) =>{
+    if(data){
+        console.log(data);
+    }
+    else{
+        console.log(err);
+    }
+
+});
+
+
+
+
+
+
