2e2620c94e408e5523315f110507afc5ff24e663
rohitrodge <webrohit27@gmail.com>
2024-04-02 09:50:29 +0530

    update callback hell

---
+++ JSON File/callback_Hell.js
@@ -0,0 +1,33 @@
+function getData(url, callback) {
+
+    const request = new XMLHttpRequest(); // state: 0
+
+    request.open('GET', url);// state: 1
+    request.send(); // state: 2
+
+    request.addEventListener('readystatechange', ()=> {
+        if(request.readyState == 4 && request.status == 200) {
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
+        }
+        else if(request.readyState == 4) {
+            callback(request.status, undefined);
+        }
+});
+}
+
+
+
+getData('todos/arijit.json', (err, data)=>{
+    console.log(data);
+
+    getData('todos/atif.json', (err, data)=>{
+        console.log(data);
+
+        getData('todos/kailash.json', (err, data)=>{
+            console.log(data);
+        });
+        
+    });
+  
+});
